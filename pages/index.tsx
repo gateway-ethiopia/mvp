7f313572656cbd4bfde9a4ef8fb5002380268233
fraol0912 <fraol0912@gmail.com>
2022-05-12 20:42:36 +0300

    fix package.json

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,11 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <div className="navbar bg-base-100 shadow-xl m-5">
-      <a className="btn btn-ghost normal-case text-xl">Gateway Ethiopia</a>
-    </div>
+    <header>
+      <div className="navbar bg-base-100 shadow-xl">
+        <a className="btn btn-ghost normal-case text-xl">Gateway Ethiopia</a>
+      </div>
+    </header>
   )
 }
 
--- pages/search.js
+++ pages/search.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import SearchBar from '../components/SearchBar'
+
 function search() {
   return (
-    <div className = 'h-screen w-screen bg-gray-300'>
-        <h1>Search</h1>
-        <div>
-            <SearchBar />
-        </div>
+    <div className="h-screen w-screen bg-gray-300">
+      <h1>Search</h1>
+      <div>
+        <SearchBar />
+      </div>
     </div>
   )
 }
