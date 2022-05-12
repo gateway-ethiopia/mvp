8f8e66ecca5ed0b0cdce40a9b9893f151ad3494a
fraol0912 <fraol0912@gmail.com>
2022-05-12 22:49:40 +0300

    add popular packages list

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,45 +1,71 @@
 import type { NextPage } from 'next'
+import Package from '../components/Package'
 
 import SearchBar from '../components/SearchBar'
 
 const Home: NextPage = () => {
   return (
-    <header>
-      <div className="fixed navbar bg-base-200">
-        <div className="flex-1">
-          <a className="btn btn-ghost normal-case text-xl">Gateway Ethiopia</a>
+    <>
+      <header>
+        <div className="fixed navbar bg-base-200">
+          <div className="flex-1">
+            <a className="btn btn-ghost normal-case text-xl">
+              Gateway Ethiopia
+            </a>
+          </div>
+          <div className="flex-none">
+            <button className="btn btn-square btn-ghost">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                className="inline-block w-5 h-5 stroke-current"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+                />
+              </svg>
+            </button>
+          </div>
         </div>
-        <div className="flex-none">
-          <button className="btn btn-square btn-ghost">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              className="inline-block w-5 h-5 stroke-current"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
-              ></path>
-            </svg>
-          </button>
-        </div>
-      </div>
 
-      <div
-        className="hero min-h-screen"
-        style={{
-          backgroundImage: 'url(/sample.jpg)',
-        }}
-      >
-        <div className="hero-overlay bg-opacity-60"></div>
-        <div className="hero-content text-center text-neutral-content h-60">
-          <SearchBar />
+        <div
+          className="hero min-h-screen"
+          style={{
+            backgroundImage: 'url(/sample.jpg)',
+          }}
+        >
+          <div className="hero-overlay bg-opacity-60"></div>
+          <div className="hero-content text-center text-neutral-content h-60">
+            <SearchBar />
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+
+      <main className="container mx-auto px-4 py-32">
+        <h1 className="text-5xl mb-10">Popular Packages</h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          <Package title="COFFEE & TEA TOUR in ETHIOPIA">
+            Drinking coffee (which in the Amharic language is called <b>buna</b>
+            ) is an essential element of culture in Ethiopia. Join us on a
+            coffee and tea trip to Ethiopia."
+          </Package>
+          <Package title="COFFEE & TEA TOUR in ETHIOPIA">
+            Drinking coffee (which in the Amharic language is called <b>buna</b>
+            ) is an essential element of culture in Ethiopia. Join us on a
+            coffee and tea trip to Ethiopia."
+          </Package>
+          <Package title="COFFEE & TEA TOUR in ETHIOPIA">
+            Drinking coffee (which in the Amharic language is called <b>buna</b>
+            ) is an essential element of culture in Ethiopia. Join us on a
+            coffee and tea trip to Ethiopia."
+          </Package>
+        </div>
+      </main>
+    </>
   )
 }
 
