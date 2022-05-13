9c786bdc1d4f2c3411a483956c4e8b1ebfaab0fd
fraol0912 <fraol0912@gmail.com>
2022-05-13 12:47:35 +0300

    fix git merge issue

---
+++ components/NavBar.tsx
@@ -0,0 +1,74 @@
+import { useEffect, useState } from 'react'
+import { SunIcon, MoonIcon } from '@heroicons/react/solid'
+import Link from 'next/link'
+
+export default function NavBar() {
+  const [darkMode, setDarkMode] = useState(false)
+
+  useEffect(() => {
+    if (darkMode) {
+      document.body.dataset.theme = 'dark'
+    } else {
+      document.body.dataset.theme = 'light'
+    }
+  }, [darkMode])
+
+  return (
+    <div className="z-30 sticky top-0 navbar bg-base-200">
+      <div className="flex-1">
+        <a className="btn btn-ghost normal-case text-xl">
+          <span className="text-primary">Gateway</span> Ethiopia
+        </a>
+      </div>
+      <div className="flex-none">
+        <button
+          className="btn btn-square btn-ghost"
+          onClick={() => setDarkMode(!darkMode)}
+        >
+          {darkMode ? (
+            <SunIcon className="w-5 fill-primary" />
+          ) : (
+            <MoonIcon className="w-5 fill-primary" />
+          )}
+        </button>
+        <div className="dropdown dropdown-end">
+          <button tabIndex={0} className="btn btn-square btn-ghost">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="inline-block w-5 h-5 stroke-current"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+              />
+            </svg>
+          </button>
+          <ul
+            tabIndex={0}
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <Link href="/">
+                <a>Home</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/travel-guide">
+                <a className="justify-between">Travel Guide Profile Page</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/search">
+                <a>Search Result Page</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
--- pages/package.tsx
+++ pages/package.tsx
@@ -11,20 +11,62 @@ export default function Package() {
       </Head>
       <NavBar />
       <section className="grid grid-cols-2">
-        <section className="py-10 px-10">
+        <section className="pt-10 pb-36 px-10 h-screen overflow-scroll">
           <h1 className="text-4xl font-bold mb-5">
             South Ethiopia Tour Omo Valley Tribes
           </h1>
 
           <section className="grid grid-cols-1 gap-4">
-            <Step num={1} title="National Museum of Ethiopia">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum
-              libero id nesciunt, repudiandae praesentium dicta quibusdam
-              expedita magnam beatae deleniti?
+            <Step
+              num={1}
+              title="Addis Ababa"
+              img="https://images.unsplash.com/photo-1624314138470-5a2f24623f10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"
+            >
+              Addis Ababa, Ethiopia's sprawling capital in the highlands
+              bordering the Great Rift Valley, is the country's commercial and
+              cultural hub. Its National Museum exhibits Ethiopian art,
+              traditional crafts and prehistoric fossils, including replicas of
+              the famous early hominid, "Lucy." The burial place of the
+              20th-century emperor Haile Selassie, copper-domed Holy Trinity
+              Cathedral, is a neo-baroque architectural landmark.
+            </Step>
+            <Step
+              num={2}
+              title="National Museum of Ethiopia"
+              img="https://itin-dev.sfo2.cdn.digitaloceanspaces.com/freeImage/https://itin-dev.sfo2.cdn.digitaloceanspaces.com/freeImage/KToNrXJo71q6sTkMmbR5IuQ2IcpKGIKB"
+            >
+              The National Museum of Ethiopia, also referred to as the Ethiopian
+              National Museum, is a national museum in Ethiopia. It is located
+              in the capital, Addis Ababa, near the Addis Ababa University's
+              graduate school.
+            </Step>
+
+            <Step
+              num={3}
+              title="Awassa"
+              img="http://t2.gstatic.com/licensed-image?q=tbn:ANd9GcQ3dYP-R5HKWzG_BGQ3APUka0TXnRTmMpltyj1mepAN1hJMuENcNPFobap8_yw9"
+            >
+              Awasa, also called Hawassa, is a city in the Great Rift Valley of
+              central Ethiopia. It lies at the eastern edge of large Lake Awasa,
+              with its resident hippos. Water birds gather around a popular fish
+              market along the lake's shore. Nearby, the waterside Amora Gedel
+              National Park is inhabited by monkeys. To the northwest, Senkele
+              Swayne’s Hartebeest Sanctuary is home to these endangered African
+              antelopes.
+            </Step>
+            <Step
+              num={4}
+              title="Arba Minch"
+              img="https://www.walkinethiopia.com/media/k2/items/cache/36fdb1a35cd2f54f95cf2119fb5bc7ed_XL.jpg"
+            >
+              Arba Minch is a city and separate woreda in the southern part of
+              Ethiopia. "Arba Minch" means "40 Springs" the name was originated
+              from the presence of more than 40 springs.
             </Step>
           </section>
         </section>
-        <section className="h-screen">
+
+        <section className="h-screen overflow-hidden">
           <iframe
             width="100%"
             height="100%"
@@ -37,10 +79,12 @@ export default function Package() {
 }
 
 function Step({
+  img,
   num,
   title,
   children,
 }: {
+  img: string
   num: number
   title: string
   children: ReactNode
@@ -64,7 +108,7 @@ function Step({
         </div>
       </div>
       <div className="col-span-1 flex items-center">
-        <img src="/gondar.jpg" className="w-fit rounded shadow-sm" />
+        <img src={img} className="w-fit rounded shadow-sm" />
       </div>
     </div>
   )
